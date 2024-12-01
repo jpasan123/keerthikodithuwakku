b07014b458ae5f1eba8cba2f39dfc1434c613b3e
Janidu Pasan Hewapathirana <138660521+jpasan123@users.noreply.github.com>
2024-12-01 07:26:25 +0530

    update-commit

--- wp-content/uploads/uag-plugin/assets/0/uag-js-32.js
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-32.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
-	UAGBCounter.init( '.uagb-block-e6f5bf6c', {"layout":"number","heading":"Year Of Experience","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":25,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+	UAGBCounter.init( '.uagb-block-e6f5bf6c', {"layout":"number","heading":"Year Of Experience","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":12,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
 });
 window.addEventListener( 'load', function() {
-	UAGBCounter.init( '.uagb-block-8a7cf7d8', {"layout":"number","heading":"Happy Clients","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":400,"totalNumber":400,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+	UAGBCounter.init( '.uagb-block-8a7cf7d8', {"layout":"number","heading":"Happy Clients","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":25,"totalNumber":25,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
 });
 window.addEventListener( 'load', function() {
 	UAGBButtonChild.init( '.uagb-block-553f0a45' );
