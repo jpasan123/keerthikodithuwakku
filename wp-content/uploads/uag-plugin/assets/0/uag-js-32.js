2ec2b2394db440c8effaf0bd4a4bda8cf9980d45
Janidu Pasan Hewapathirana <138660521+jpasan123@users.noreply.github.com>
2025-07-02 23:53:35 +0530

    update to commit

--- wp-content/uploads/uag-plugin/assets/0/uag-js-32.js
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-32.js
@@ -1,7 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
-	UAGBCounter.init( '.uagb-block-e6f5bf6c', {"layout":"number","heading":"Year Of Experience","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":12,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
-});
-				window.addEventListener( 'DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function(){ 				window.addEventListener( 'DOMContentLoaded', function() {
 					const scope = document.querySelector( '.uagb-block-a16a7725' );
 					if ( scope ){
 						if ( scope.children[0].classList.contains( 'spectra-image-gallery__layout--masonry' ) ) {
